82d0b41390963dc936e92cbe5d5ed63b42593a23
nurettin senyer <nurettin.senyer@bil.omu.edu.tr>
2010-11-06 03:15:05 +0200

    demo altinda toplandi, pathos

--- src/demo_gait_analysis.m
+++ src/demo_gait_analysis.m
@@ -8,16 +8,15 @@ isCreateBW = false;
 % % dbnm = strcat(DB_ROOT(), 'gait/soton/');
 dbnm = '../../db/hepsi/';
 
-e = dir(strcat(dbnm, 'e*'));
-k = dir(strcat(dbnm, 'k*'));
+e = dir(pathos(strcat(dbnm, 'e*')));
+k = dir(pathos(strcat(dbnm, 'k*')));
 ek = cat(1, e, k);
 ds = size(ek);
 
 for d=1:ds,
     dnm = ek(d).name;
     if dbg, fprintf('dir nm: %s\n', dnm);   end
-    bw_dbnm = strcat(dbnm, dnm, '/');
-    bw_dbnm = strrep(bw_dbnm, '/', filesep);
+    bw_dbnm = pathos(strcat(dbnm, dnm, '/'));
 
     if isCreateBW
         bg = bgmodel(dbnm, dbg);
@@ -84,4 +83,46 @@ for d=1:ds,
     [cfrm, cS, cE] = cycle_crop(frm, W, false);
     cw = w(:, cS:cE);
     cR = R(:,:, cS:cE);
+    
+    % CG: tum numuneler, CC: tum numunelerin siniflari
+    
+    CG(d, 1) = mean(cw(1, :));%mean
+    CG(d, 2) = mean(cw(2, :));%mean
+    CG(d, 3) = mean(cw(3, :));%mean
+    CG(d, 4) = mean(cw(4, :));%mean
+    CG(d, 5) = mean(cR(1, 3, :)) / mean(cR(1, 4, :));
+    CG(d, 6) = mean(cR(1, 5, :));
+    CG(d, 7) = abs(max(cR(2, 1, :)) - max(cR(3, 1, :)));
+    CG(d, 8) = abs(max(cR(4, 1, :)) - max(cR(5, 1, :)));
+    CG(d, 9) = abs(max(cR(6, 1, :)) - max(cR(7, 1, :)));
+    CG(d, 10) = max(cR(7, 5, :)) / min(cR(7, 5, :));
+    
+    if (dnm(1) == 'e')
+        CC(d) = 0;
+    elseif (dnm(1) == 'k')
+        CC(d) = 1;
+    else
+        CC(d) = -1;
+    end
+    CC = CC';
 end
+
+% 0 ile 1 arasinda normalizasyon
+for i=1:size(CG, 2)
+    if min(CG(:, i)) < 0
+       CG(:, i) = CG(:, i) + abs(min(CG(:, i)));
+       CG(:, i) = CG(:, i) / max(CG(:, i));
+    else
+       CG(:, i) = CG(:, i) - min(CG(:, i));
+       CG(:, i) = CG(:, i) / max(CG(:, i));
+    end
+end
+
+% normalizasyon sonu
+
+C = CG; C(:, i+1) = CC; % nitelik-sinif bir arada C' nin icerisinde.
+
+dlmwrite(pathos(strcat(dbnm, 'ds/dataset.bak')), CG, ',');
+dlmwrite(pathos(strcat(dbnm, 'ds/dsclass.bak')), CC, ',');
+
+lvq2_emre();
