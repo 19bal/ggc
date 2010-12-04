0d86338c0a8158ab8cd409844d63aecccf83f687
nurettin senyer <nurettin.senyer@bil.omu.edu.tr>
2010-12-04 04:55:10 +0200

    svm eklentisi: OK

---
+++ src/ggc_svm_test.m
@@ -0,0 +1,24 @@
+function [trn, tst] = ggc_svm_test(dbnm_ds, dbg)
+% function [trn, tst] = ggc_svm_test(dbnm_ds, dbg)
+
+[trn_ds, tst_ds] = data_sets([], dbnm_ds, dbg);
+
+load(pathos('_bkp/model_svm.mat'));
+
+[trn.err, trn.perf] = helper(trn_ds, model, dbg);
+[tst.err, tst.perf] = helper(tst_ds, model, dbg);
+
+
+% local function
+function [err, perf] = helper(ds, model, dbg)
+
+data.X = cat(1, ds.X)';
+data.y = 1 + cat(1, ds.y)';
+data.Etiket = {'E', 'K'};
+
+y = svmclass(data.X, model);
+
+if dbg, Etiket = [data.Etiket(data.y'); data.Etiket(y')]',  end
+
+err = mean(abs(data.y - y).^2);
+perf = sum((data.y - y) == 0) / length(y);
---
+++ src/ggc_svm_train.m
@@ -0,0 +1,19 @@
+function ggc_svm_train(dbnm_ds, dbg)
+% function ggc_svm_train(dbnm_ds, dbg)
+
+[trn_ds, tst_ds] = data_sets([], dbnm_ds, dbg);
+
+% data yi bsvm nin arzu ettigi forma donustur
+data.X = cat(1, trn_ds.X)';
+data.y = 1 + cat(1, trn_ds.y)';
+data.Etiket = {'E', 'K'};
+
+options.solver = 'imdm';
+model = bsvm2(data, options);
+
+curdir = pwd;
+    dnm = pathos('_bkp/');
+    mkdir(dnm);
+    cd(dnm);
+    save model_svm net
+cd(curdir);
