1baa51bc77d130b8a87235f052b463a02c895c74
Nurettin Senyer <seyyah@bil.omu.edu.tr>
2010-12-27 16:51:37 +0200

    null

--- 4-classification/runme.m
+++ 4-classification/runme.m
@@ -7,17 +7,17 @@ dbg = true;
 dbnm_ds = pathos('_ds/emre/');
 fprintf('DS: %s\n', dbnm_ds);
 
-% LVQ
-tic;
-ggc_lvq_train(dbnm_ds, dbg);
-[lvq_trn, lvq_tst] = ggc_lvq_test(dbnm_ds, dbg)
-ct_lvq = toc                    % computation time
+        % LVQ
+        tic;
+        ggc_lvq_train(dbnm_ds, dbg);
+        [lvq_trn, lvq_tst] = ggc_lvq_test(dbnm_ds, dbg)
+        ct_lvq = toc                    % computation time
 
-% SVM
-tic;
-ggc_svm_train(dbnm_ds, dbg);
-[svm_trn, svm_tst] = ggc_svm_test(dbnm_ds, dbg)
-ct_svm = toc                    % computation time
+        % SVM
+        tic;
+        ggc_svm_train(dbnm_ds, dbg);
+        [svm_trn, svm_tst] = ggc_svm_test(dbnm_ds, dbg)
+        ct_svm = toc                    % computation time
 
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
 
@@ -25,15 +25,15 @@ ct_svm = toc                    % computation time
 dbnm_ds = pathos('_ds/bauckhage09/');
 fprintf('DS: %s\n', dbnm_ds);
 
-% LVQ
-tic;
-ggc_lvq_train(dbnm_ds, dbg);
-[lvq_trn, lvq_tst] = ggc_lvq_test(dbnm_ds, dbg)
-ct_lvq = toc                    % computation time
+        % LVQ
+        tic;
+        ggc_lvq_train(dbnm_ds, dbg);
+        [lvq_trn, lvq_tst] = ggc_lvq_test(dbnm_ds, dbg)
+        ct_lvq = toc                    % computation time
 
-% SVM
-tic;
-ggc_svm_train(dbnm_ds, dbg);
-[svm_trn, svm_tst] = ggc_svm_test(dbnm_ds, dbg)
-ct_svm = toc                    % computation time
+        % SVM
+        tic;
+        ggc_svm_train(dbnm_ds, dbg);
+        [svm_trn, svm_tst] = ggc_svm_test(dbnm_ds, dbg)
+        ct_svm = toc                    % computation time
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
