47149bb41c4a66b062e445fec818e072c191c02f
Nurettin Senyer <seyyah@bil.omu.edu.tr>
2011-01-03 13:39:51 +0200

    yineleme secenegi

--- 4-classification/runme.m
+++ 4-classification/runme.m
@@ -5,63 +5,79 @@ dbg = true;
 fprintf('LVQ - SVM\n');
 fprintf('   ct  | trn | tst ||   ct   | trn | tst |\n');
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
-% % % % % % emre
-dbnm_ds = pathos('_ds/emre/');
 
-        % LVQ
-        tic;
-        ggc_lvq_train(dbnm_ds, dbg);
-        [lvq_trn, lvq_tst] = ggc_lvq_test(dbnm_ds, dbg);
-        ct_lvq = toc;                    % computation time
+for i = 1:10
+    fprintf('%d. yineleme...\n', i);
+    
+    % % % % % % emre
+    dbnm_ds = pathos('_ds/emre/');
 
-        % SVM
-        tic;
-        ggc_svm_train(dbnm_ds, dbg);
-        [svm_trn, svm_tst] = ggc_svm_test(dbnm_ds, dbg);
-        ct_svm = toc;                    % computation time
+            % LVQ
+            tic;
+            ggc_lvq_train(dbnm_ds, dbg);
+            [lvq_trn, lvq_tst] = ggc_lvq_test(dbnm_ds, dbg);
+            ct_lvq = toc;                    % computation time
 
-        fprintf('%6.3f | %03d | %03d || %6.3f | %03d | %03d |\t%s\n', ...
-            ct_lvq, max(lvq_trn.perf)*100, max(lvq_tst.perf)*100, ...
-            ct_svm, max(svm_trn.perf)*100, max(svm_tst.perf)*100, dbnm_ds);
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+            % SVM
+            tic;
+            ggc_svm_train(dbnm_ds, dbg);
+            [svm_trn, svm_tst] = ggc_svm_test(dbnm_ds, dbg);
+            ct_svm = toc;                    % computation time
 
-% % % % % % bauckhage09
-dbnm_ds = pathos('_ds/bauckhage09/');
+    %         fprintf('%6.3f | %03d | %03d || %6.3f | %03d | %03d |\t%s\n', ...
+    %             ct_lvq, max(lvq_trn.perf)*100, max(lvq_tst.perf)*100, ...
+    %             ct_svm, max(svm_trn.perf)*100, max(svm_tst.perf)*100, dbnm_ds);
 
-        % LVQ
-        tic;
-        ggc_lvq_train(dbnm_ds, dbg);
-        [lvq_trn, lvq_tst] = ggc_lvq_test(dbnm_ds, dbg);
-        ct_lvq = toc;                    % computation time
+            emre_CT_SVM(i) = ct_svm;
+            emre_TRN_LVQ(i) = max(lvq_trn.perf)*100;
 
-        % SVM
-        tic;
-        ggc_svm_train(dbnm_ds, dbg);
-        [svm_trn, svm_tst] = ggc_svm_test(dbnm_ds, dbg);
-        ct_svm = toc;                    % computation time
-        
-        fprintf('%6.3f | %03d | %03d || %6.3f | %03d | %03d |\t%s\n', ...
-            ct_lvq, max(lvq_trn.perf)*100, max(lvq_tst.perf)*100, ...
-            ct_svm, max(svm_trn.perf)*100, max(svm_tst.perf)*100, dbnm_ds);
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+    % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
 
-% % % % % % bauckhage09
-dbnm_ds = pathos('_ds/felez10/');
+    % % % % % % bauckhage09
+    dbnm_ds = pathos('_ds/bauckhage09/');
 
-        % LVQ
-        tic;
-        ggc_lvq_train(dbnm_ds, dbg);
-        [lvq_trn, lvq_tst] = ggc_lvq_test(dbnm_ds, dbg);
-        ct_lvq = toc;                    % computation time
+            % LVQ
+            tic;
+            ggc_lvq_train(dbnm_ds, dbg);
+            [lvq_trn, lvq_tst] = ggc_lvq_test(dbnm_ds, dbg);
+            ct_lvq = toc;                    % computation time
 
-        % SVM
-        tic;
-        ggc_svm_train(dbnm_ds, dbg);
-        [svm_trn, svm_tst] = ggc_svm_test(dbnm_ds, dbg);
-        ct_svm = toc;                    % computation time
+            % SVM
+            tic;
+            ggc_svm_train(dbnm_ds, dbg);
+            [svm_trn, svm_tst] = ggc_svm_test(dbnm_ds, dbg);
+            ct_svm = toc;                    % computation time
 
-        fprintf('%6.3f | %03d | %03d || %6.3f | %03d | %03d |\t%s\n', ...
-            ct_lvq, max(lvq_trn.perf)*100, max(lvq_tst.perf)*100, ...
-            ct_svm, max(svm_trn.perf)*100, max(svm_tst.perf)*100, dbnm_ds);
-        
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+    %         fprintf('%6.3f | %03d | %03d || %6.3f | %03d | %03d |\t%s\n', ...
+    %             ct_lvq, max(lvq_trn.perf)*100, max(lvq_tst.perf)*100, ...
+    %             ct_svm, max(svm_trn.perf)*100, max(svm_tst.perf)*100, dbnm_ds);
+            bauckhage09_CT_SVM(i) = ct_svm;
+            bauckhage09_TRN_LVQ(i) = max(lvq_trn.perf)*100;
+            
+    % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+
+    % % % % % % felez10
+    dbnm_ds = pathos('_ds/felez10/');
+
+            % LVQ
+            tic;
+            ggc_lvq_train(dbnm_ds, dbg);
+            [lvq_trn, lvq_tst] = ggc_lvq_test(dbnm_ds, dbg);
+            ct_lvq = toc;                    % computation time
+
+            % SVM
+            tic;
+            ggc_svm_train(dbnm_ds, dbg);
+            [svm_trn, svm_tst] = ggc_svm_test(dbnm_ds, dbg);
+            ct_svm = toc;                    % computation time
+
+    %         fprintf('%6.3f | %03d | %03d || %6.3f | %03d | %03d |\t%s\n', ...
+    %             ct_lvq, max(lvq_trn.perf)*100, max(lvq_tst.perf)*100, ...
+    %             ct_svm, max(svm_trn.perf)*100, max(svm_tst.perf)*100, dbnm_ds);
+            felez10_CT_SVM(i) = ct_svm;
+            felez10_TRN_LVQ(i) = max(lvq_trn.perf)*100;
+    % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+end
+
+felez10_ct_svm  = mean(felez10_CT_SVM)
+felez10_trn_lvq = mean(felez10_TRN_LVQ)
